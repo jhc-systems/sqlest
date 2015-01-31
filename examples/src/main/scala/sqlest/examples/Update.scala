968b3f1857d4be6edf7f1316ea2e53f521715053
mishyTheBear <hamish.dickson@gmail.com>
2015-01-31 15:38:26 +0000

    Demonstrate update must be run in a transaction

--- examples/src/main/scala/sqlest/examples/Update.scala
+++ examples/src/main/scala/sqlest/examples/Update.scala
@@ -10,6 +10,13 @@ object UpdateExamples extends App with DatabaseExample {
       .set(FruitTable.juiciness -> 9)
       .where(FruitTable.name === """Banana""")
 
+  // Write operations must be run in a transaction - the below will throw an exception
+  try {
+    updateStatement.execute
+  } catch {
+    case e: AssertionError => println(e.getMessage)
+  }
+  
   database.withTransaction {
     // running execute on the update statement returns the number of lines changed (ie updated)
     val numberInsert = updateStatement.execute
