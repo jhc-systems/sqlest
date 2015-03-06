7cf4e4d57c29bf5d61a26879cdfb9626d52bc2c6
Brendan Maginnis <brendan.maginnis@jhc.co.uk>
2015-03-06 10:29:02 +0000

    Version 0.6.1

--- project/Build.scala
+++ project/Build.scala
@@ -66,7 +66,7 @@ object SqlestBuild extends Build {
 
   def commonSettings = SbtScalariform.scalariformSettings ++ Seq(
     organization := "uk.co.jhc",
-    version := "0.6.0",
+    version := "0.6.1",
     scalaVersion := "2.11.5",
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Xfatal-warnings", "-language:implicitConversions", "-language:existentials")
   )
