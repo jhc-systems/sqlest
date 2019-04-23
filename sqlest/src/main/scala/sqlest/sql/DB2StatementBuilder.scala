dadf1f3d263e69145faad6fddf40587b68c879ba
Alex Chazot <alex.chazot@jhc.co.uk>
2019-04-23 11:36:24 +0100

    Revert OFFSET (due to clash with existing SQL Functions)

--- sqlest/src/main/scala/sqlest/sql/DB2StatementBuilder.scala
+++ sqlest/src/main/scala/sqlest/sql/DB2StatementBuilder.scala
@@ -57,28 +57,19 @@ trait DB2StatementBuilder extends base.StatementBuilder {
     }
 
   override def selectSql(select: Select[_, _ <: Relation]): String = {
-    Seq(
-      selectWhatSql(select.columns),
-      selectFromSql(select.from)
-    ) ++ Seq(
-        selectWhereSql(select.where),
-        selectStartWithSql(select.startWith),
-        selectConnectBySql(select.connectBy),
-        selectGroupBySql(select.groupBy),
-        selectHavingSql(select.having),
-        selectOrderBySql(select.orderBy),
-        selectOffsetSql(select.offset),
-        selectLimitSql(select.limit),
-        selectOptimizeSql(select.optimize),
-        selectUnionSql(select.union)
-      ).flatten mkString (" ")
+    val offset = select.offset getOrElse 0L
+    if (offset > 0L) {
+      rowNumberSelectSql(select, offset, select.limit)
+    } else {
+      super.selectSql(select)
+    }
   }
 
   override def selectLimitSql(limit: Option[Long]): Option[String] =
     limit map (limit => s"fetch first $limit rows only")
 
   override def selectOffsetSql(offset: Option[Long]): Option[String] =
-    offset map (offset => s"offset ${literalSql(offset)} rows")
+    None
 
   override def selectOptimizeSql(optimize: Option[Long]): Option[String] =
     optimize map (optimize => s"optimize for $optimize rows")
@@ -91,6 +82,25 @@ trait DB2StatementBuilder extends base.StatementBuilder {
     case _ => super.joinSql(relation)
   }
 
+  def rowNumberSelectSql(select: Select[_, _ <: Relation], offset: Long, limit: Option[Long]): String = {
+    val subquery = Seq(
+      s"${selectWhatSql(select.columns)}, row_number() over (${selectOrderBySql(select.orderBy) getOrElse ""}) as rownum",
+      selectFromSql(select.from)
+    ) ++ Seq(
+        selectWhereSql(select.where),
+        selectGroupBySql(select.groupBy)
+      ).flatten mkString " "
+
+    val what =
+      select.columns map (col => identifierSql(col.columnAlias)) mkString ", "
+
+    val bounds = limit
+      .map(limit => s"rownum between ? and ?")
+      .getOrElse(s"rownum >= ?")
+
+    s"with subquery as ($subquery) select $what from subquery where $bounds"
+  }
+
   override def columnSql(column: Column[_]): String =
     column match {
       case literalColumn: LiteralColumn[_] if literalColumn.columnType == BooleanColumnType =>
@@ -100,11 +110,37 @@ trait DB2StatementBuilder extends base.StatementBuilder {
       case _ => super.columnSql(column)
     }
 
+  override def selectArgs(select: Select[_, _ <: Relation]): List[LiteralColumn[_]] = {
+    val offset = select.offset getOrElse 0L
+    if (offset > 0L) {
+      rowNumberSelectArgs(select, offset, select.limit)
+    } else {
+      super.selectArgs(select)
+    }
+  }
+
   override def selectLimitArgs(limit: Option[Long]): List[LiteralColumn[_]] =
+    Nil
+
+  override def selectOffsetArgs(limit: Option[Long]): List[LiteralColumn[_]] =
     Nil
 
   override def selectOptimizeArgs(optimize: Option[Long]): List[LiteralColumn[_]] =
     Nil
+
+  def rowNumberSelectArgs(select: Select[_, _ <: Relation], offset: Long, limit: Option[Long]): List[LiteralColumn[_]] = {
+    val subqueryArgs =
+      selectWhatArgs(select.columns) ++
+        selectOrderByArgs(select.orderBy) ++
+        selectFromArgs(select.from) ++
+        selectWhereArgs(select.where)
+
+    val boundsArgs = limit
+      .map(limit => List(LiteralColumn[Long](offset + 1), LiteralColumn[Long](offset + limit)))
+      .getOrElse(List(LiteralColumn[Long](offset + 1)))
+
+    subqueryArgs ++ boundsArgs
+  }
 
   override def columnArgs(column: Column[_]): List[LiteralColumn[_]] = column match {
     case column: LiteralColumn[_] if column.columnType == BooleanColumnType => List(LiteralColumn(0), LiteralColumn(0))
